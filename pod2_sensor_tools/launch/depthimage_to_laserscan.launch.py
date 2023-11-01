a8017604a078042f868e8bd80efc478b397a887f
Rak-r <rakshitnar12@gmail.com>
2023-11-01 13:32:16 +0000

    Add the depthimage_to_lasercscan package

---
+++ pod2_sensor_tools/launch/depthimage_to_laserscan.launch.py
@@ -0,0 +1,17 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    param_config = os.path.join(
+        get_package_share_directory('depthimage_to_laserscan'), 'config', 'param.yaml')
+    return LaunchDescription([
+        Node(
+            package='depthimage_to_laserscan',
+            executable='depthimage_to_laserscan_node',
+            name='depthimage_to_laserscan',
+            remappings=[('depth', '/camera/depth/image_rect_raw'),
+                        ('depth_camera_info', '/camera/depth/camera_info')],
+            parameters=[param_config])
+    ])
