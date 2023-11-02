5a2dfc2b37c60f527ade4b7a562b30535e1fdcda
Rak-r <rakshitnar12@gmail.com>
2023-11-02 12:52:22 +0000

    update depth cam node

--- pod2_description/ROS2_Time_Nodes/Depth_image_2_real.py
+++ pod2_description/ROS2_Time_Nodes/Depth_image_2_real.py
@@ -4,20 +4,20 @@ from rclpy.node import Node
 from sensor_msgs.msg import Image,CameraInfo
 
 '''In order to avoid time related errors and tf errors this node is created to convert the messages coming from gazebo to wall time. 
-This makes sure that the whole system works on same clock. The node susbcribes the lidar scan topic from GZ laser plugin and publishes to ROS2 topic /scan
+This makes sure that the whole system works on same clock. The node susbcribes the depth, rgb image and camera info topic from GZ camera plugin and publishes to ROS2 topic /scan
 with changing the time stamp to wall time.
 
 Usage:
 cd ros_ws
 source install/setup.bash
-ros2 run pod2_description laser_sim2real.py
+ros2 run pod2_description Depth_image_2_real.py
 
 First time Errors:
 
 If terminal returns no executable found, it may be to provide permission to execute the file first time.
 Steps:
-cd ros_ws/src/pod2_description/scripts
-chmod +x laser_sim2real.py
+cd ros_ws/src/pod2_description/ROS2_Time_Nodes
+chmod +x Depth_image_2_real.py
 cd ../../..
 Try the run command again.
 
