21330d5d2c9938ef07fe85d0139629f20c1bc5d9
Rak-r <rakshitnar12@gmail.com>
2025-01-06 14:01:41 +0530

    clean the launch file

--- src/pod2_navigation/launch/OpenPodCar_NAV2.launch.py
+++ src/pod2_navigation/launch/OpenPodCar_NAV2.launch.py
@@ -80,7 +80,7 @@ def generate_launch_description():
         DeclareLaunchArgument('use_sim_time', default_value='false', description='Use simulation (Gazebo) clock if true'),
 
         DeclareLaunchArgument('autostart', default_value='true', description='Automatically startup the nav2 stack'),
-        # nav2_dwb_smac nav2_game_theory
+       
         DeclareLaunchArgument('params_file', default_value=os.path.join(bringup_dir, 'config', 'nav2_dwb_smac.yaml'), description='Full path to the ROS2 parameters file to use'),
 
     
